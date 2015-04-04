29c967913a7bf460d810671ced6c51d10ff26c8a
bartolomeon <root@reprepro.c4c.sprint.pl>
2015-04-04 03:12:34 +0200

    returns json

--- reprepro-rest-client.js
+++ reprepro-rest-client.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var sh = require('shelljs');
-var q = require('q')
+var Q = require('q')
 var _ = require('underscore')
 
 var npmConfig = require('./package.json');
@@ -12,20 +12,15 @@ var reprepro = {
   EXEC_NAME : "reprepro",
 
   verifyPackageName : function(packageName) {
-    var deferred = q.defer();
-    if (  /^[\x00-\x7F;]+$/.test(packageName) && /^[^;\s]/.test(packageName) ) {
-	deferred.resolve();
-    } else {
-      deferred.reject(new Error('Invalid package name format given!'));
-    }
-    return deferred.promise;
+    return /^[\x00-\x7F;]+$/.test(packageName) && /^[^;\s]/.test(packageName);
   },
 
   findPackage : function(packageName) {
-    var deferred = q.defer();
+    var deferred = Q.defer();
 
     sh.cd(config.reprepro.homeDir);
     sh.exec('reprepro ls "'+packageName+'"', function(status, result) {
+
       if (!status) { //0 = success
 	deferred.resolve(result);
       } else {
@@ -50,28 +45,32 @@ var reprepro = {
 };
 
 reprepro.checkIfInstalled();
+app.get('/reprepro/:packagename', function(req, res) {
+  var packageName = req.params.packagename;
+
+   if (!reprepro.verifyPackageName(packageName)) {
+	res.status(400).send('Invalid characters used');
+   } else {
+     reprepro.findPackage(packageName)
+     .then(function(result) {
+	var out =
+	  _.filter(result.split('\n'), function(line) {return line.length>0;})
+	  .map(function(line) { return _.invoke(line.split('|'), 'trim').slice(1,2); } );
+	res.send({ versions : _.flatten(out) } );
+     }).fail(function(error) {
+	res.status(400).send(JSON.stringify(error));
+     }).done();
+  } 
+});
 
 
-app.get('/', function (req, res) {
+
+app.get('*', function (req, res) {
   var routes = 
     _.filter(app._router.stack, function(el) {return el.route;})
   .map(function(el){ return el.route })
   .map(function(el){ return JSON.stringify(el)+'<br>'});
   res.send(npmConfig.name + ' ' + npmConfig.version + '<br><br>' + routes );
-});
-
-app.get('/reprepro/:packagename', function(req, res) {
-  var packageName = req.params.packagename;
-
-  reprepro.verifyPackageName(packageName)
-  .then(reprepro.findPackage(packageName))
-  .then( function(result) {
-	res.send(result);
-  }).catch(function(error) {
-	res.status = 400;
-	res.send(JSON.stringify(error));
-  }).done();
-
 });
 
 
